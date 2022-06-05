07db56d49aa59275b3f3c585c53ae5b517d14d39
Сигов Дмитрий <99177871+SigovDmitriy@users.noreply.github.com>
2022-06-05 19:15:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,11 +6,5 @@
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-  std::string s = tree[n - 1];
-    std::vector<char> der;
-    for (int k = 0; k < s.length();) {
-        der.push_back(s[k]);
-        k++;
-    }
-    return der;
+//Напишите реализацию
 }
